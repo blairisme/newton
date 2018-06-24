4520eb0d0688711e36138816fc16fd62c462d5a9
Blair <blair.butterworth@gmail.com>
2018-06-24 09:02:26 +0100

    Improved security by using password encoding

--- src/main/java/org/ucl/newton/application/SecurityConfiguration.java
+++ src/main/java/org/ucl/newton/application/SecurityConfiguration.java
@@ -11,12 +11,15 @@ package org.ucl.newton.application;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
@@ -43,42 +46,33 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
                 .and()
             .logout()
                 .permitAll();
-                //.and().exceptionHandling().accessDeniedPage("/Access_Denied");
+                //.and().exceptionHandling().accessDeniedPage("/access_denied");
+
+                //.antMatchers("/admin/*").hasAnyRole("ROLE_ADMIN")
 
     }
 
-//    @Bean
-//    @Override
-//    protected UserDetailsService userDetailsService() {
-//        UserDetails user = User
-//                .withUsername("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user);
-//    }
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-//    @Inject
-//    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-//        BCryptPasswordEncoder encoder = passwordEncoder();
-//        auth.inMemoryAuthentication().withUser("user").password(encoder.encode("password")).roles("ADMIN");
-//    }
-//
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    protected DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
 
     @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
         return new InMemoryUserDetailsManager(user);
     }
 }
