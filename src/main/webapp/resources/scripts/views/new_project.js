8570bd590b3d53c6583180cf64364d43b1432687
Blair <blair.butterworth@gmail.com>
2018-06-30 18:53:18 +0100

    Fixed some codacy style warnings

--- src/main/webapp/resources/scripts/views/new_project.js
+++ src/main/webapp/resources/scripts/views/new_project.js
@@ -11,38 +11,42 @@
 
 $(document).ready(function() {
     var matchingUsers = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace("value"),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
 
         remote: {
-            url: '/api/user?matching=%QUERY',
-            wildcard: '%QUERY'
+            url: "/api/user?matching=%QUERY",
+            wildcard: "%QUERY"
         }
     });
 
-    $('#projectMemberInput').typeahead(null, {
-        name: 'users',
-        display: 'name',
+    $("#projectMemberInput").typeahead(null, {
+        name: "users",
+        display: "name",
         minLength: 3,
         source: matchingUsers,
         templates: {
-            empty: ['<span class="empty-message">No matching users</span>'].join('\n'),
-            suggestion: Handlebars.compile('<div> <img src="/resources/images/profile.jpg" class="rounded-circle avatar" alt="Profile picture"/> <span><strong>{{name}}</strong> ({{email}})</span></div>')
+            empty: `<span class="empty-message">No matching users</span>`,
+            suggestion: Handlebars.compile(
+                `<div>
+                    <img src="/resources/images/profile.jpg" class="rounded-circle avatar" alt="Profile picture"/>
+                    <span><strong>{{name}}</strong> ({{email}})</span>
+                </div>`)
         }
     });
 
-    $('#projectMemberInput').bind('typeahead:select', function(ev, member) {
+    $("#projectMemberInput").bind("typeahead:select", function(ev, member) {
         addMember(member.id, member.name, member.email);
     });
 });
 
 function addMember(id, name, email) {
-    $('#projectMemberInput').typeahead('close');
-    $('#projectMemberInput').typeahead('val', '');
+    $("#projectMemberInput").typeahead("close");
+    $("#projectMemberInput").typeahead("val", "");
 
-    $('#projectMembersListEmpty').hide();
+    $("#projectMembersListEmpty").hide();
 
-    $('#projectMembersList').append(
+    $("#projectMembersList").append(
         `<li id="listItem${id}" class="list-group-item project-list-item">
             <img src="/resources/images/profile.jpg" class="rounded-circle avatar" alt="Profile picture"/>
             <span>${name} (${email})</span>
@@ -50,7 +54,7 @@ function addMember(id, name, email) {
         </li>`
     );
 
-    $('#projectMembersData').append(
+    $("#projectMembersData").append(
         `<option id="dataItem${id}" value="${id}" selected="selected"></option>`
     );
 }
@@ -59,7 +63,7 @@ function removeMember(id) {
     $(`#listItem${id}`).remove();
     $(`#dataItem${id}`).remove();
 
-    if ($('#projectMembersList li').length == 0) {
-        $('#projectMembersListEmpty').show();
+    if ($("#projectMembersList li").length == 0) {
+        $("#projectMembersListEmpty").show();
     }
 }
