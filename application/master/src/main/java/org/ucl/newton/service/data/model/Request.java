a6ad996f294a68c0844f7ea8d045e23fef260355
Xiaolong <cxlcxl008@tju.edu.cn>
2018-07-04 13:10:42 +0100

    add code to serialize the weather data from json to bean

---
+++ application/master/src/main/java/org/ucl/newton/service/data/model/Request.java
@@ -0,0 +1,22 @@
+package org.ucl.newton.service.data.model;
+
+public class Request {
+    private String type;
+    private String query;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
