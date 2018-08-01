aaf4f0d9f32915559e735f85da360db48cb65684
j-wilkie <john.wilkie.17@ucl.ac.uk>
2018-08-01 20:09:17 +0100

    Fix for star/unstar project bug

--- application/master/src/main/webapp/resources/scripts/views/project/details.js
+++ application/master/src/main/webapp/resources/scripts/views/project/details.js
@@ -2,10 +2,11 @@ $(document).ready( function() {
 
     $("#starBtn").click( function() {
         var type = $(this).text();
+        var id = $("#identifier").val();
         $(this).prop("disabled", true);
         $.ajax({
             type: 'POST',
-            url: "/project/project-fizzyo?type=" + type,
+            url: "/project/" + id + "?type=" + type,
             success: function(data) {
                 if(type === "Star") {
                     $("#starBtn").html("Unstar");
