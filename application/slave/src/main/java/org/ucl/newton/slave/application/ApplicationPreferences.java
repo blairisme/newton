7bd06a28c0f280e2e351e2334fc0b5008f805c58
ziad-halabi9 <ziad.halabi9@gmail.com>
2018-08-03 12:11:55 +0100

    hardcode master ip

--- application/slave/src/main/java/org/ucl/newton/slave/application/ApplicationPreferences.java
+++ application/slave/src/main/java/org/ucl/newton/slave/application/ApplicationPreferences.java
@@ -30,7 +30,8 @@ public class ApplicationPreferences
     }
 
     public String getMasterHost() {
-        return getProperty("newton.master.host", "master-newton");
+        return "104.41.220.38";
+      //  return getProperty("newton.master.host", "master-newton");
     }
 
     public int getMasterPort() {
