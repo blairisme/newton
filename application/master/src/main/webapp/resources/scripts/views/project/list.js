df08d965548a4b6e43590de64398827f5f45d902
j-wilkie <john.wilkie.17@ucl.ac.uk>
2018-08-05 19:37:30 +0100

    Added automatic default icons

---
+++ application/master/src/main/webapp/resources/scripts/views/project/list.js
@@ -0,0 +1,13 @@
+$(document).ready( function() {
+    $(".default-icon-text").each(function( ) {
+       $(this).css("background-color", getBgColor($(this).text()));
+    });
+});
+
+var colors = ["#00467d","#c3f73a","#95e06c","#68b684","#1c1018","#a8dadc","#457b9d","#e63946","#721817","#fa9f42","#541388", "#ffd400", "#d90368"];
+
+function getBgColor(letter) {
+    var index = letter.charCodeAt(0);
+    index = index % colors.length;
+    return colors[index];
+}
