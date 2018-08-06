de58bf90afaf7bd2bdef80379e70c3927b05a928
ziad-halabi9 <ziad.halabi9@gmail.com>
2018-08-06 20:43:08 +0100

    removed private slave constructor and jar

--- application/slave/src/main/java/org/ucl/newton/slave/application/Application.java
+++ application/slave/src/main/java/org/ucl/newton/slave/application/Application.java
@@ -21,9 +21,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application
 {
-    private Application() {
-        throw new UnsupportedOperationException();
-    }
+   // private Application() {
+   //     throw new UnsupportedOperationException();
+   // }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
