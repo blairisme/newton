78f39e69fb2c2dd7a5008d1597f6bf666c02fcb7
ziad-halabi9 <ziad.halabi9@gmail.com>
2018-08-08 12:37:42 +0100

    ziad user

--- deployment/jupyter/config/jupyterhub_config.py
+++ deployment/jupyter/config/jupyterhub_config.py
@@ -51,7 +51,7 @@ c.NewtonAuthenticator.forward_url = 'spawn'
 c.LocalAuthenticator.create_system_users = True
 
 if platform.system() == 'Linux':
-    c.LocalAuthenticator.add_user_cmd = ['adduser', '--group', 'wheel', '--create-home']
+    c.LocalAuthenticator.add_user_cmd = ['adduser', 'ziad', '--create-home']
 
 if platform.system() == 'Darwin':
     script_path = path.join(current_path, "adduser")
