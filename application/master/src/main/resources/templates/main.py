05accb02f22988c7ddb8ae2ccce2d3bf2a20fbc3
ziad-halabi9 <ziad.halabi9@gmail.com>
2018-08-16 17:10:30 +0100

    print(2)

--- application/master/src/main/resources/templates/main.py
+++ application/master/src/main/resources/templates/main.py
@@ -1,13 +1,4 @@
 
 # TODO - Replae with your own code
 
-import numpy as np
-import csv
-
-x = np.linspace(-1, 1, 100)
-y = x**2
-
-# Saves the x axis values to a file named output1.csv
-with open('output1.csv', 'w') as myfile:
-    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    wr.writerow(x)
+print(2)
