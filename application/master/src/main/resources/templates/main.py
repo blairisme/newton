4423332da21c727a00fb4285ce2063fe104d8b80
ziad-halabi9 <ziad.halabi9@gmail.com>
2018-08-16 16:50:55 +0100

    changed python default script

--- application/master/src/main/resources/templates/main.py
+++ application/master/src/main/resources/templates/main.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-# TODO - Replae with your own code 
-import matplotlib.pyplot as plt; plt.rcdefaults()
+
+# TODO - Replae with your own code
+
 import numpy as np
-import matplotlib.pyplot as plt
 import csv
 
 x = np.linspace(-1, 1, 100)
 y = x**2
-plt.plot(x,y)
-plt.savefig('fig1.png') # Saves the graph to a file named fig1.png
 
 # Saves the x axis values to a file named output1.csv
-with open('output1.csv', 'wb') as myfile:
+with open('output1.csv', 'w') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     wr.writerow(x)
