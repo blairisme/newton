73105791a6fdaafadabb56f344781120eb89779f
Blair <blair.butterworth@gmail.com>
2018-08-18 09:21:36 +0100

    Added resources and security tweaks

--- application/master/src/main/java/org/ucl/newton/application/security/SecurityConfiguration.java
+++ application/master/src/main/java/org/ucl/newton/application/security/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 
     private void setAuthorizedPaths(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/signup", "/resources/**").permitAll()
+                .antMatchers("/", "/signup", "/resources/**", "/.well-known/**").permitAll()
                 .antMatchers("/settings/**").hasAnyRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/api/**").hasAnyRole("API", "ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/api/**").hasAnyRole("API", "ADMIN")
