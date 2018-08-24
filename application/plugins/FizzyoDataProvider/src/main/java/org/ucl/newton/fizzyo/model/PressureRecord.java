629b36e295739e445487affa0f80b4b1c1d68067
Xiaolong <cxlcxl008@tju.edu.cn>
2018-08-24 13:49:03 +0100

    add test for Fizzyo Data Provider

--- application/plugins/FizzyoDataProvider/src/main/java/org/ucl/newton/fizzyo/model/PressureRecord.java
+++ application/plugins/FizzyoDataProvider/src/main/java/org/ucl/newton/fizzyo/model/PressureRecord.java
@@ -42,24 +42,4 @@ public class PressureRecord implements FizzyoDataUnit{
         values.add(averageBreathLength);
         return values;
     }
-    public int getGoodBreaths() { return goodBreaths; }
-    public void setGoodBreaths(int goodBreaths) { this.goodBreaths = goodBreaths; }
-    public String getBreaths() { return breaths; }
-    public void setBreaths(String breaths) { this.breaths = breaths; }
-    public String getAverageBreathLength() { return averageBreathLength; }
-    public void setAverageBreathLength(String averageBreathLength) { this.averageBreathLength = averageBreathLength; }
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getStartTime() { return startTime; }
-    public void setStartTime(String startTime) { this.startTime = startTime; }
-    public String getEndTime() { return endTime; }
-    public void setEndTime(String endTime) { this.endTime = endTime; }
-    public String getAveragePressure() { return averagePressure; }
-    public void setAveragePressure(String averagePressure) { this.averagePressure = averagePressure; }
-    public String getPressureRawId() { return pressureRawId; }
-    public void setPressureRawId(String pressureRawId) { this.pressureRawId = pressureRawId; }
-    public String getPatientRecordId() { return patientRecordId; }
-    public void setPatientRecordId(String patientRecordId) { this.patientRecordId = patientRecordId; }
-
-
 }
