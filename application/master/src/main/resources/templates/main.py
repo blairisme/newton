d48cfb87622f4063b9bfe0572456c0a0afd46e09
ziad-halabi9 <ziad.halabi9@gmail.com>
2018-08-28 16:57:15 +0100

    image pull policy + reset python default script

--- application/master/src/main/resources/templates/main.py
+++ application/master/src/main/resources/templates/main.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+# TODO - Replace with your own code
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
 
-# TODO - Replae with your own code
-import math
+x = np.linspace(-1, 1, 100)
+y = x**2
+plt.plot(x,y)
+plt.savefig('fig1.png') # Saves the graph to a file named fig1.png
 
-for x in range(20000):
-    y = x**x*x
-    z = math.sqrt(x)
-
-
-print ("z=",z)
+# Saves the x axis values to a file named output1.csv
+with open('output1.csv', 'w') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(x)
